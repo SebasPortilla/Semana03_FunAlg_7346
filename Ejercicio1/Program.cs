fb1417767706e7b95eebf02a46ef2f8d260c78ef
Sebastian <Sebastian@upn.pe>
2025-04-08 19:10:59 -0500

    semana 3 ejercicio 1

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -10,6 +10,20 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            int numero1;
+            int numero2;
+            int suma;
+            //Console.ReadKey();
+
+            Console.Write("Ingrese numero 1: ");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese numero 2: ");
+            numero2 = int.Parse(Console.ReadLine());
+            Console.Write("La suma es: ");
+            suma = numero1 + numero2;
+            Console.WriteLine(suma);
+
+
         }
     }
 }
